d171dfabda8f81dcde4c568b6429d09d81594dfb
NikolayNMD <136238601+NikolayNMD@users.noreply.github.com>
2023-10-05 22:29:04 +0300

    1st commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector('.gallery');
+const itemsLoad = createGalleryItemsLoad(items);
+
+galleryDiv.insertAdjacentElement('beforeend', items);
+galleryDiv.addEventListener('click', onImgClick);
+
+function createGalleryItemsLoad(item) {
+    return item.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+      })
+      .join('');
+  }
