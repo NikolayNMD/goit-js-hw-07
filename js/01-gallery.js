9fedf3cec9fa527d0bd7a6c8adeceadc7871ae6a
NikolayNMD <136238601+NikolayNMD@users.noreply.github.com>
2023-10-05 22:31:56 +0300

    2cnd commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,8 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+console.log(galleryItems);
+
 const galleryDiv = document.querySelector('.gallery');
 const itemsLoad = createGalleryItemsLoad(items);
 
