e50daa3c22e38a4cc29036895c7eb604e2cf66fb
NikolayNMD <136238601+NikolayNMD@users.noreply.github.com>
2023-10-07 00:50:10 +0300

    1 task 1 save

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,26 +1,57 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryDiv = document.querySelector('.gallery');
-const itemsLoad = createGalleryItemsLoad(items);
+const itemsMarkup = createGalleryItemsLoad(galleryItems);
 
-galleryDiv.insertAdjacentElement('beforeend', items);
+
+galleryDiv.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createGalleryItemsLoad(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+
 galleryDiv.addEventListener('click', onImgClick);
 
-function createGalleryItemsLoad(item) {
-    return item.map(({ preview, original, description }) => {
-        return `<div class="gallery__item">
-    <a class="gallery__link" href="${original}">
-      <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-      />
-    </a>
-  </div>`;
-      })
-      .join('');
+function onImgClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return;
+
+  const isItemImage = e.target.classList.contains('gallery__image');
+  if (!isItemImage) return;
+
+  const currentImgUrl = e.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+		<img src="${currentImgUrl}" width="1280" height="auto"/>
+        `,
+    {
+      onShow: (instance) => {
+        window.addEventListener('keydown', onEscKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener('keydown', onEscKeyPress);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscKeyPress(e) {
+    if (e.code !== 'Escape') return;
+    instance.close();
   }
+}
