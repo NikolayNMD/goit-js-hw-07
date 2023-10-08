39237b6f18f65d85df0e26199aa1789158b2479a
NikolayNMD <136238601+NikolayNMD@users.noreply.github.com>
2023-10-08 23:38:06 +0300

    2 task done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryDiv = document.querySelector('.gallery');
+
+const markup = galleryItems.reduce(
+    (acc, { original, preview, description }) =>
+        (acc += `<li>
+    <a class="gallery__item" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+        />
+    </a>
+    </li>`),
+    ''
+    );
+
+galleryDiv.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
